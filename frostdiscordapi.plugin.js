a88682706439c969ee05c34e8e5cfdab8ed75e49
Krizfrost <dejkriz@outlook.com>
2017-11-02 07:59:29 -0700

    Plugin API Launch

---
+++ frostdiscordapi.plugin.js
@@ -0,0 +1,16 @@
+.message-group:not(.compact) .avatar-large[style*="117280061236707332"] ~ .comment .message h2 span.username-wrapper::after, .channel-members .member .avatar-small[style="117280061236707332"] ~ .member-inner .member-username .member-username-inner::after {
+    content: "Owner";
+    color: #fff;
+    border-radius: 3px;
+    background: #8213E4;
+    margin-left: 6px;
+    font-size: 10px;
+    font-weight: 600;
+    text-transform: uppercase;
+    vertical-align: bottom;
+    padding: 1px 4px;
+    line-height: 14px;
+    flex-shrink: 0;
+    -webkit-flex-shrink: 0;
+    text-shadow: rgba(0, 0, 0, 0.4) 1px 1px 1px;
+}
